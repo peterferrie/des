1720ca6525e66f4f51b7392c0b970cf7ac113b00
Odzhan <odzhan@protonmail.com>
2015-12-13 07:42:13 +0000

    des

--- bld32.bat
+++ bld32.bat
@@ -1,8 +1,8 @@
 @echo off
 yasm -fwin32 dx.asm -odx.obj
 yasm -fbin -DBIN dx.asm -odx.bin
-cl /nologo /O2 /Os /Fa /GS- des_test.c des.c des_cbc.c
+cl /nologo /O2 /Os /GS- des_test.c des.c des_cbc.c
 move des_test.exe c_test.exe
-cl /DUSE_ASM /nologo /O2 /Os /Fa /GS- des_test.c des_cbc.c dx.obj
+cl /DUSE_ASM /nologo /O2 /Os /GS- des_test.c des_cbc.c dx.obj
 move des_test.exe asm_test.exe
 del *.obj *.err
